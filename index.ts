3d259db9aa7e80558da03d68a7c2d00b18b84a02
pwojton <piotr.wojton.01@gmail.com>
2024-04-20 18:16:53 +0200

    Add veryfication of Action and Resource to verifyStatement

--- index.ts
+++ index.ts
@@ -3,6 +3,14 @@ import { IAMRolePolicy, PolicyDocument, Statement } from './types';
 
 const path = './data.json';
 
+function isArrayOfType<T>(arr: any[], predicate: (value: any) => value is T): arr is T[] {
+  return Array.isArray(arr) && arr.every(predicate);
+}
+
+function isString(value: any): value is string {
+  return typeof value === 'string';
+}
+
 const readIAMPolicyFromJSONFile = async (filePath: string): Promise<IAMRolePolicy> => {
   const data = await fsPromises.readFile(filePath, { encoding: 'utf8' });
   const policy = JSON.parse(data);
@@ -10,11 +18,16 @@ const readIAMPolicyFromJSONFile = async (filePath: string): Promise<IAMRolePolic
   return policy;
 };
 
-const verifyStatement = (statement: Statement): boolean => {
-  const { Effect, Resource } = statement;
+const verifyStatement = (statement: Statement): statement is Statement => {
+  const { Effect, Resource, Action } = statement;
 
   if (!(Effect === 'Allow' || Effect === 'Deny')) {
     console.error('Effect must be either Allow or Deny');
+    return false;
+  }
+
+  if (!(typeof Resource === 'string' || isArrayOfType<string>(Resource, isString))) {
+    console.error('Resource must be a string or an array of strings');
     return false;
   }
 
@@ -23,10 +36,20 @@ const verifyStatement = (statement: Statement): boolean => {
     return false;
   }
 
+  if (!(typeof Action === 'string' || isArrayOfType<string>(Action, isString))) {
+    console.error('Action must be a string or an array of strings');
+    return false;
+  }
+
   return true;
 };
 
 const verifyIAMPolicyProperties = (policyName: string, policyDocument: PolicyDocument): boolean => {
+  if (typeof policyName !== 'string' || typeof policyDocument !== 'object') {
+    console.error('PolicyName must be a string');
+    return false;
+  }
+
   const policyNamePattern = /^[\w+=,.@-]+$/;
   if (!policyNamePattern.test(policyName)) {
     console.error('PolicyName does not match the pattern');
@@ -36,6 +59,7 @@ const verifyIAMPolicyProperties = (policyName: string, policyDocument: PolicyDoc
     console.error('PolicyName must be between 1 and 128 characters long');
     return false;
   }
+
   if (!policyDocument) {
     console.error('PolicyDocument is required');
     return false;
